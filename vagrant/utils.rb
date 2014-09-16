d32f4f07a8df5fd241c30a6fbe29c6dbc406cb8f
Yorrick Jansen <yorrick.jansen@gmail.com>
2014-09-16 14:57:36 -0400

    nginx and others

--- vagrant/utils.rb
+++ vagrant/utils.rb
@@ -1,11 +1,18 @@
-# adds sync folders to given VM
-def sync_folders(vm)
+# returns a Hash(app_name, host_app_path)
+def get_host_apps()
     if ENV.has_key?('PYANSIBLE_APPS')
         apps = ENV['PYANSIBLE_APPS'].split(',').to_set
+
+        return Hash[apps.map { |host_app_path| [host_app_path.split("/").last, host_app_path] }]
+
         apps.each do |host_app_path|
             app_name = host_app_path.split("/").last
             # vm.synced_folder host_app_path, "/srv/app/#{app_name}", :owner=> 'webapp', :group=>'webapp', :mount_options => ['dmode=777', 'fmode=777']
             vm.synced_folder host_app_path, "/srv/app/#{app_name}", :mount_options => ['dmode=777', 'fmode=777']
         end
+
+
+    else
+        return {}
     end
 end
